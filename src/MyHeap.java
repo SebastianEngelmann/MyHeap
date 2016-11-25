94996de78afc68cd765b75cc63ab3d1813f578d4
Sebastian Engelmann <s.engelmann@fu-berlin.de>
2016-11-25 22:59:38 +0100

    MyHeap -Update

--- src/MyHeap.java
+++ src/MyHeap.java
@@ -2,4 +2,16 @@
  * Created by sebastian on 25.11.16.
  */
 public class MyHeap {
+    // Attribute
+    int[] array;
+    int lastElem;
+
+    // Konstruktor
+    public MyHeap(int n)
+    {
+        this.array = new int[n];
+        this.lastElem = -1;
+    }
+
+    // Methoden
 }
