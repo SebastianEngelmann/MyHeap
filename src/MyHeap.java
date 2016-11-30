7ae43c72843cde44b0fd2f1c0c5daa88cc619179
Sebastian Engelmann <s.engelmann@fu-berlin.de>
2016-11-30 20:45:41 +0100

    MyHeap -Update

--- src/MyHeap.java
+++ src/MyHeap.java
@@ -1,56 +1,111 @@
 /**
  * Created by sebastian on 25.11.16.
  */
+
 public class MyHeap {
     // Attribute
     private int[] array;
     private int lastElem;
+    private int root;
 
     // Konstruktor
     public MyHeap(int n)
     {
         this.array = new int[n];
-        this.lastElem = -1;
+        this.lastElem = -1; // Zeiger auf Index des letzten besetzten Elementes
+        this.root = 0; // Index der Wurzel
     }
 
     // Methoden
-    public void insert(int k)
+    public void insert(int k) throws Exception
     {
-        if(lastElem == array.length - 1)
+        if(lastElem == array.length - 1) // Array voll?
         {
-            return;
+            throw new Exception("Heap ist ueberfuellt!");
         }
         else
         {
-            increaseLastElem();
-            array[lastElem] = k;
-            bubbleUp(k);
+            increaseLastElem(); // erhoehe lastElem um 1
+            array[lastElem] = k; // speichere k an der Stelle lastElem im Array
+            bubbleUp(lastElem); // bubbleUp falls noetig
         }
     }
 
-    public int deleteMin()
+    public int deleteMin() throws Exception
     {
-        return 0;
+        if(lastElem == -1)
+        {
+            throw new Exception("Heap ist leer!");
+        }
+        else
+        {
+            int result = array[0];
+            array[0] = array[lastElem]; // ersetze Wurzel mit letztem Element im Heap
+            decreaseLastElem(); // veringere lastElem um 1
+            bubbleDown(root);
+            return result;
+        }
     }
 
     private void bubbleDown(int k)
     {
+        if(array[root] < array[getLeftChildIndex(root)] && array[root] < array[getRightChildIndex(root)]) // Wurzel kleiner als Kinder?
+        {
+            return; // breche ab
+        }
+        else
+        {
+            int help = root;
+            while((hasLeftChild(help) == true || hasRightChild(help)) == true && (less(getLeftChildIndex(help), help) || less(getParentIndex(help), help)))
+            {
+                if(less(getLeftChildIndex(help), getRightChildIndex(help)) == true) // linkes Kind von root kleiner als rechtes Kind von root?
+                {
+                    swap(getLeftChildIndex(help), help); // tausche linkes Kind mit Elternknoten
+                    help = getLeftChildIndex(help);
+                }
+                if(less(getRightChildIndex(help), getLeftChildIndex(help)) == true) // rechtes Kind von root groesser als linkes Kind von root?
+                {
+                    swap(getRightChildIndex(help), help); // tausche rechtes Kind mit Elternknoten
+                    help = getRightChildIndex(help);
+                }
+            }
+        }
+    }
 
+    private void swap(int i, int j)
+    {
+        int tmp = array[i]; // speichere das i-te Element im Array in tmp
+        array[i] = array[j]; // ueberschreibe das i-te Element mit dem j-ten Element im Array
+        array[j] = tmp; // ueberschreibe das j-te Element mit tmp im Array
+    }
+
+    private boolean less(int i, int j)
+    {
+        if(i > lastElem || j > lastElem)
+        {
+            return false;
+            //break;
+        }
+        else
+        {
+            return array[i] < array[j]; // wenn das i-te Element < j-te Element im Array, gib true zurueck
+        }
     }
 
     private void bubbleUp(int k)
     {
-        if(getParentIndex(lastElem) < 0)
+        if(getParentIndex(lastElem) < 0) // Index des Elternknotens von lastElem < 0 ?
         {
-            System.out.println("OK");
-            return;
+            return; // breche ab
         }
-        if(k < array[getParentIndex(lastElem)])
+        else
         {
-            int tmp = array[getParentIndex(lastElem)];
-            array[getParentIndex(lastElem)] = k;
-            array[lastElem] = tmp;
-            bubbleUp(array[getParentIndex(lastElem)]);
+            int help = lastElem;
+            while(help >= 0 && getParentIndex(help) >= 0 && less(help, getParentIndex(help))== true) // Elternknoten < Kindknoten?
+            {
+                swap(getParentIndex(help), help); // tausche Elternknoten mit Kindknoten
+                help = getParentIndex(help); //
+            }
         }
     }
 
@@ -71,7 +126,12 @@ public class MyHeap {
 
     private void increaseLastElem()
     {
-        this.lastElem++;
+        this.lastElem++; // erhoehe lastElem um 1
+    }
+
+    private void decreaseLastElem()
+    {
+        this.lastElem--; // veringere lastElem um 1
     }
 
     private boolean hasLeftChild(int k)
@@ -84,11 +144,40 @@ public class MyHeap {
         return (2 * k + 2 <= lastElem)? true:false;
     }
 
-    private boolean hasParent(int k)
+    public void updateKey(int i, int j)
     {
-        // tba
-        return true;
+        int index = findKey(i);
+        array[index] = j; // ersetze Schluessel i durch Schluessel j
+        int ls = lastElem;
+        int r = root;
+        if(array[index] < array[getParentIndex(index)])
+        {
+            lastElem = index;
+            bubbleUp(index);
+            lastElem = ls;
+        }
+        if((getRightChildIndex(index) <= lastElem && array[index] > array[getRightChildIndex(index)]) || (getLeftChildIndex(index) <= lastElem && array[index] > array[getLeftChildIndex(index)]))
+        {
+            root = index;
+            bubbleDown(index);
+            root = r;
+        }
     }
+
+    public int findKey(int i)
+    {
+        int result = -1;
+        for(int j = 0; j < lastElem + 1; j++)
+        {
+            if(i == array[j]) // zu ersetzender Schluessel im Array?
+            {
+                result = j; // gib Index zurueck
+                break;
+            }
+        }
+        return result;
+    }
+
 
     public void print()
     {
@@ -96,5 +185,6 @@ public class MyHeap {
         {
             System.out.print(array[i] + " ");
         }
+        System.out.println("\n");
     }
 }
--- src/RunApp.java
+++ src/RunApp.java
@@ -2,15 +2,20 @@
  * Created by sebastian on 25.11.16.
  */
 public class RunApp {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws Exception {
         MyHeap myHeap = new MyHeap(6);
-        myHeap.insert(5);
+        myHeap.insert(6);
         myHeap.insert(3);
         myHeap.insert(2);
         myHeap.insert(1);
-        //myHeap.insert(8);
-        //myHeap.insert(8);
+        myHeap.insert(8);
+        myHeap.insert(0);
+        //myHeap.print();
+        //myHeap.deleteMin();
+        //myHeap.print();
+        //myHeap.updateKey(2, 0);
+        //myHeap.print();
+        myHeap.updateKey(1, 9); // der Fall stimmt noch nicht
         myHeap.print();
     }
 }
